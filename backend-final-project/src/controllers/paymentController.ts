24db9e05e7da45f23e43dcb49fc2730e783196d1
princegtpinn <itsaree.n9@gmail.com>
2025-02-10 12:40:31 +0700

    Update package and install dotenv

--- backend-final-project/src/controllers/paymentController.ts
+++ backend-final-project/src/controllers/paymentController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import Stripe from "stripe";
 import nodemailer from "nodemailer";
-
 import {
   createOrderNotLoginUser,
   findNotLoginOrderByOrderId,
@@ -21,6 +20,7 @@ import {
   updateProductAfterSold,
   updateProductTotalSale,
 } from "../services/productService";
+require("dotenv").config();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
